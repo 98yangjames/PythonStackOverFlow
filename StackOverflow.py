bebe2a2639f378d770c700bf3a3558e054eff6c7
arik-shurygin <arik.shurygin@gmail.com>
2022-02-21 12:26:28 -0800

    creating all of the method stubs for our project
    created method stubs and did some research on how we can modify builtin library code to create custom exceptions.

---
+++ StackOverflow.py
@@ -0,0 +1,42 @@
+import requests
+
+'''
+This is a class which searches and returns information about queries to stack overflow. 
+removing the need to search for things on a web browser by handling everything within the IDE
+
+
+NOTE: we will probably need to extend the print_exception() method inside of python/lib/traceback.py for our project.
+'''
+class StackOverflow():
+
+    #todo, decide whether or not we want the user to specifiy a search string upon initialization of the class, or later
+    # in the search method below.
+    def __init__(self, search_string):
+        print("initializing...")
+        self.meta_data = {}
+        self.stack_data = {}
+
+    #calls the stackoverflow API to find posts that include a given search string, then chooses the post_chosen'th
+    #post to save information and meta data on.
+    # PARAMS: search_string, an string describing what is being searched on stack overflow, similar to how a user
+    #                       might search stack overflow on the website.
+    # PARAMS: post_chosen, a search string will most likely return more than one post, which post should be selected
+    #                      is decided based on the priority that they are returned in the Stackoverflow API
+    def search(self, search_string, post_chosen=0):
+        #todo use requests to search stack overflow API, store appropriate data in self.meta_data and self.stack_data
+
+    # gets the title to the post searched
+    def get_title(self):
+        #todo get the title of the post via self.stack_data
+
+    #gets the content written by the author of the post, usually describing the problem they are facing.
+    def get_content(self):
+        #todo get the content of the post from self.stack_data
+
+    #returns the URL of the stack overflow post searched
+    def get_url(self):
+        #todo get the link via self.meta_data
+
+    #returns all of the comments underneath the post searched.
+    def get_comments(self):
+        #todo return the comments under the post via information in self.stack_data
