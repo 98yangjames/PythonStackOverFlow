be165bbead614e6bd9909b6e67c7edf7beb590d1
arik-shurygin <arik.shurygin@gmail.com>
2022-03-01 11:35:10 -0800

    Adding functionality to get the basic information and do a search on stackapi
    can now search the API to find a question and link it, but lack the question text and the comments answering it.

--- StackOverflow.py
+++ StackOverflow.py
@@ -1,4 +1,12 @@
 import requests
+import traceback
+import sys
+import json
+
+
+stack_data_keys = ['tags', 'owner', 'is_answered', 'view_count', 'accepted_answer_id', 'answer_count', 'score',
+                   'last_activity_date', 'creation_date', 'last_edit_date', 'question_id', 'content_license', 'link',
+                   'title']
 
 '''
 This is a class which searches and returns information about queries to stack overflow. 
@@ -9,12 +17,16 @@ NOTE: we will probably need to extend the print_exception() method inside of pyt
 '''
 class StackOverflowAPI():
 
-    #todo, decide whether or not we want the user to specifiy a search string upon initialization of the class, or later
-    # in the search method below.
     def __init__(self, exception):
-        print("initializing...")
+        self.BASEURL = "https://api.stackexchange.com/2.2/"
         self.meta_data = {}
         self.stack_data = {}
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        tb = traceback.TracebackException(exc_type, exc_value, exc_tb)
+        self.search(''.join(tb.format_exception_only()))
+
+        #TODO ADD THE INFO NEEDED TO THE STACKTRACE AND THEN RERAISE THE EXCEPTION WITH THE INFO ATTACHED
+
 
     #calls the stackoverflow API to find posts that include a given search string, then chooses the post_chosen'th
     #post to save information and meta data on.
@@ -23,27 +35,44 @@ class StackOverflowAPI():
     # PARAMS: post_chosen, a search string will most likely return more than one post, which post should be selected
     #                      is decided based on the priority that they are returned in the Stackoverflow API
     def search(self, search_string, post_chosen=0):
-        print("todo")
+        print("searching")
         #todo use requests to search stack overflow API, store appropriate data in self.meta_data and self.stack_data
+        url = self.BASEURL + "search"
+        params = {
+            "site": "stackoverflow",
+            "intitle": search_string,
+            "order": "desc",
+            "sort": "votes",
+            "tagged": "python"
+        }
+        r = requests.get(url, params=params)
+        self.meta_data = {"Status Code": r.status_code, "API URL": r.url, "HEADERS": r.headers}
+        self.stack_data = list(json.loads(r.content)['items'])[0]
+
+        #TODO GET THIS TO WORK SO THAT WE CAN GET THE QUESTION ITSELF IN OUR STACKTRACE
+        url_answers = self.BASEURL + "questions/{"+ str(self.stack_data['question_id'])+ ";}/answers"
+        print(url_answers)
+        r = requests.get(url_answers)
+        print(r)
+        #print(self.stack_data)
+
 
     # gets the title to the post searched
     def get_title(self):
-        print("todo")
-        #todo get the title of the post via self.stack_data
+        return self.stack_data['title']
 
     #gets the content written by the author of the post, usually describing the problem they are facing.
     def get_content(self):
-        print("todo")
+        return None
         #todo get the content of the post from self.stack_data
 
     #returns the URL of the stack overflow post searched
     def get_url(self):
-        print("todo")
-        #todo get the link via self.meta_data
+        return self.stack_data['link']
 
     #returns all of the comments underneath the post searched.
     def get_comments(self):
-        print("todo")
+        return None
         #todo return the comments under the post via information in self.stack_data
 
 
@@ -56,5 +85,5 @@ b = 0
 try:
     c = a/b
 except Exception as e:
-    StackOverflowAPI(e) #for now this does nothing with the exception
+    a = StackOverflowAPI(e) #for now this does nothing with the exception
 
